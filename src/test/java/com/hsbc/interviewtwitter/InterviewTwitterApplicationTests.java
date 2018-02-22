4c87a5f0b93548b78f53ee7c2193a9d30490882e
Saeid <saeidrzm@gmail.com>
2018-02-22 20:01:11 +0100

    INIT#Init commit. Generated via spring initializer + cassandra unit

---
+++ src/test/java/com/hsbc/interviewtwitter/InterviewTwitterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hsbc.interviewtwitter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InterviewTwitterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
